aa3f1ac98b8bcc6850847f138215abce2f67ceb6
vuquyen <vuvanquyen2110@gmail.com>
2016-12-27 17:23:41 +0700

    create login admin

---
+++ resources/views/admin/index1.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quan Tri</title>
+    <meta name='viewport' content='width'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('css.login')}}">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.js"></script>
+</head>
+<body>
+    @include('admin.login')
+</body>
+</html>
